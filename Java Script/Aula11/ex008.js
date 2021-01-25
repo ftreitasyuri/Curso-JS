3efe42f99bbe6f45979a95fcbad969f7def63d93
Yuri Freitas <yuriftq@gmail.com>
2021-01-25 01:27:36 -0300

    Atualização Aual 11
    Trabalhando com condiçoes simples e compostas.

---
+++ Java Script/Aula11/ex008.js
@@ -0,0 +1,12 @@
+// console.log('O console funcionou corretamente')
+
+var vel = 55
+
+console.log(`A velocidade do seu carro é ${vel}KM/H`)
+
+if (vel > 60) { //condição simples
+        console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ Java Script/Aula11/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'Brasil'
+
+console.log(`Vivendo em ${pais} você é`)
+
+if (pais == 'Brasil'){ // condição composta
+    console.log('Brasileiro')
+}
+else{
+    console.log('Estrangeiro')
+}
