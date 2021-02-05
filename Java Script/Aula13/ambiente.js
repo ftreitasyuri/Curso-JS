77917f85ef01d08ef663d873ca3d540df3db21f4
Yuri Freitas <yuriftq@gmail.com>
2021-02-05 20:02:32 -0300

    loops
    conhecendo while e doWhile

---
+++ Java Script/Aula13/ambiente.js
@@ -0,0 +1,24 @@
+// primeiro exemplo sem while
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+
+// INICIO WHILE
+var contador = 1
+
+while (contador < 7){
+    console.log(`Passo ${contador}`)
+    contador = contador + 1 // ou contador++
+}
+// FIM IWHILE
+
+// INICIO DO WHILE
+
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+
+} while (contador < 7)
+// FIM DO WHILE
