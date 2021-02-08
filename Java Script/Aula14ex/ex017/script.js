9feabcde25216b9491aee41de3d2daad7697c1b0
Yuri Freitas <yuriftq@gmail.com>
2021-02-08 19:13:13 -0300

    add Exercicio
    tabuada com while

--- Java Script/Aula14ex/ex017/script.js
+++ Java Script/Aula14ex/ex017/script.js
@@ -1,1 +1,32 @@
-// alert('Olá')
+function validar(){
+
+    var mult = document.getElementById('num')
+    var res = document.getElementById('seltab')
+    
+
+    //  var numero = Number(mult.value)
+    
+
+    if (mult.value.length == 0){
+        
+        window.alert('Número muito grande tente um menor.')
+    }
+   
+    else{
+        
+        let numero = Number(mult.value)
+        let contador  = 1
+        
+        res.innerHTML = ''
+        while (contador <= 10){
+            let item = document.createElement('option')
+            item.text = `${numero} X ${contador} = ${numero*contador}`
+            res.appendChild(item)
+            contador++
+        }
+
+    }
+
+
+     
+}
